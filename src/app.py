99430af88eac4f4231b9ca15fc59a01ea45f4e67
shahilprasad <shahil.prasad17s@gmail.com>
2023-12-12 19:59:07 +1100

    Updated app.py to include blueprint for db commands.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from os import environ
 from init import *
+from blueprints.cli_bp import db_cmd
 
 def create_app():
     # Initialize app by creating flask app object
@@ -10,7 +11,9 @@ def create_app():
     app.config['JWT_SECRET_KEY'] = environ.get('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
 
-    # Initialized object from init file for use
+    # Initialized object from init file for use 
     db.init_app(app)
 
+    app.register_blueprint(db_cmd)
+    
     return app
