e451a76aba041083ce5dc2498bd1d7bceec87e68
shahilprasad <shahil.prasad17s@gmail.com>
2023-12-16 20:04:57 +1100

    Updated authorisation function to include user created only authorisation.

--- src/auth.py
+++ src/auth.py
@@ -3,13 +3,13 @@ from models.user import User
 from init import db
 from flask import abort
 
-# Create a function for admin only functions
-def admin_only():
+# Create a function for admin or user only functions
+def authorization(user_id=None):
     # Retrieve susername from token
-    user_id = get_jwt_identity()
+    jwt_user_id = get_jwt_identity()
     # Query the database for a user with the given username
-    stmt = db.select(User).filter_by(id=user_id)
+    stmt = db.select(User).filter_by(id=jwt_user_id)
     user = db.session.scalar(stmt)
-    # If user is not an admin, abort
-    if not user.is_admin:
-        abort(401)
+    # If user is not an admin, or the user that created the resource, abort
+    if not (user.is_admin or (user_id and jwt_user_id == user_id)):
+        abort(401, description="You are not authorized to perform this action")
